2aa6b315d10346a78a495c80ebdd4415ab0f0fd6
Arushi Arora <arushiarora2302@gmail.com>
2017-10-17 22:18:21 +0530

    list item

--- app/src/main/java/com/aarushi/sunshine/MainActivity.java
+++ app/src/main/java/com/aarushi/sunshine/MainActivity.java
@@ -2,9 +2,11 @@ package com.aarushi.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvWeatherDescription,tvDate,tvWeatherLow,tvWeatherHigh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
